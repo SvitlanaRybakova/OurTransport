260d570574be87d6aec8e4a3738c05b0bf4af86d
Svitlana Rybakova <rsv0011@gmail.com>
2021-01-03 17:50:09 +0100

    implement modal windows

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+const orderConfirm = document.querySelector('#orderConfirm');
+const calculateTheCost = document.querySelector('#calculateTheCost');
+
+orderConfirm.addEventListener('click', () => {
+  calculateTheCost.style.display = "none";
+})
