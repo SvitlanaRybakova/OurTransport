b718160e3e1d77963a5a70e9d3763bb6b132465f
Svitlana Rybakova <rsv0011@gmail.com>
2021-01-05 20:36:35 +0100

    fixed backdrop second attempt

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,10 +4,12 @@ const closeModals = document.querySelector('#close_modals');
 
 
 confirmBtn.addEventListener('click', () => {
+  console.log('in');
   calculateTheCost.style.display = "none"; 
 });
 
 closeModals.addEventListener('click', () => {
   const modalBackdrop = document.querySelector('.modal-backdrop');
   modalBackdrop.style.visibility = 'hidden';
-}  
+  location.reload();
+});  
